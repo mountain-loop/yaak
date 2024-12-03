f820179e4b5446e0725d2d7eac07c775d27c399f
Gregory Schier <gschier1990@gmail.com>
2024-12-03 06:55:28 -0800

    Update

--- src-web/components/Settings/SettingsLicense.tsx
+++ src-web/components/Settings/SettingsLicense.tsx
@@ -2,10 +2,13 @@ import { useLicense } from '@yaakapp-internal/license';
 import { format, formatDistanceToNow } from 'date-fns';
 import { open } from '@tauri-apps/plugin-shell';
 import React, { useState } from 'react';
+import { useSettings } from '../../hooks/useSettings';
 import { useToggle } from '../../hooks/useToggle';
 import { Banner } from '../core/Banner';
 import { Button } from '../core/Button';
 import { Icon } from '../core/Icon';
+import { InlineCode } from '../core/InlineCode';
+import { Link } from '../core/Link';
 import { PlainInput } from '../core/PlainInput';
 import { HStack, VStack } from '../core/Stacks';
 
@@ -13,6 +16,9 @@ export function SettingsLicense() {
   const { check, activate } = useLicense();
   const [key, setKey] = useState<string>('');
   const [activateFormVisible, toggleActivateFormVisible] = useToggle(false);
+  const settings = useSettings();
+  const specialAnnouncement =
+    settings.createdAt < '2024-12-02' && check.data?.type === 'trial_ended';
 
   return (
     <div className="flex flex-col gap-4">
@@ -23,7 +29,7 @@ export function SettingsLicense() {
         </Banner>
       )}
       {check.data?.type === 'trial_ended' && (
-        <Banner color="primary">
+        <Banner color={'primary'}>
           <strong>Your trial ended on {format(check.data.end, 'MMMM dd, yyyy')}</strong>. A
           commercial-use license is required if you use Yaak within a for-profit organization of two
           or more people.
@@ -38,6 +44,28 @@ export function SettingsLicense() {
 
       {check.error && <Banner color="danger">{check.error}</Banner>}
       {activate.error && <Banner color="danger">{activate.error}</Banner>}
+
+      {specialAnnouncement && (
+        <VStack className="my-4 max-w-lg" space={4}>
+          <p>
+            <strong>Thank you for being an early supporter of Yaak!</strong>
+          </p>
+          <p>
+            To support the ongoing development of the best local-first API client, Yaak now requires
+            a paid license for the commercial use of prebuilt binaries (personal use and running the
+            open-source code remains free.)
+          </p>
+          <p>
+            For details, see the{' '}
+            <Link href="https://yaak.app/blog/commercial-use-license">Announcement Post</Link>.
+          </p>
+          <p>
+            As a thank-you, enter code <InlineCode>EARLYAAK</InlineCode> at checkout for 50% off
+            your first year of the individual plan.
+          </p>
+          <p>~ Greg</p>
+        </VStack>
+      )}
 
       {check.data?.type === 'commercial_use' ? (
         <HStack space={2}>
