5825d5e4febd58f5f51ccb75f9962a42b5d6a6bd
Gregory Schier <gschier1990@gmail.com>
2024-12-19 05:51:01 -0800

    Update count badge style

--- src-web/components/core/CountBadge.tsx
+++ src-web/components/core/CountBadge.tsx
@@ -12,10 +12,11 @@ export function CountBadge({ count, className }: Props) {
       aria-hidden
       className={classNames(
         className,
+        'flex items-center',
         'opacity-70 border border-border-subtle text-4xs rounded mb-0.5 px-1 ml-1 h-4 font-mono',
       )}
     >
-      {count === true ? '⊙' : count}
+      {count === true ? <div aria-hidden className="rounded-full h-1 w-1 bg-text-subtle" /> : count}
     </div>
   );
 }
