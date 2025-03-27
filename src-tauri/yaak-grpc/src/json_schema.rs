7cd3b8f2e3485ba09d1f0cbbb2435af213c40e80
Hao Xiang <haoxiangzhao@outlook.com>
2025-03-27 11:07:14 +0800

    fix proto to json-schema

--- src-tauri/yaak-grpc/src/json_schema.rs
+++ src-tauri/yaak-grpc/src/json_schema.rs
@@ -1,16 +1,16 @@
-use prost_reflect::{DescriptorPool, MessageDescriptor};
-use prost_types::field_descriptor_proto;
-use serde::{Deserialize, Serialize};
+use prost_reflect::{DescriptorPool, FieldDescriptor, MessageDescriptor};
+use serde::Serialize;
 use std::collections::HashMap;
 
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Default, Serialize)]
 #[serde(default, rename_all = "camelCase")]
 pub struct JsonSchemaEntry {
     #[serde(skip_serializing_if = "Option::is_none")]
     title: Option<String>,
 
-    #[serde(rename = "type")]
-    type_: JsonType,
+    // ref no need type
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
+    type_: Option<JsonType>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     description: Option<String>,
@@ -18,18 +18,164 @@ pub struct JsonSchemaEntry {
     #[serde(skip_serializing_if = "Option::is_none")]
     properties: Option<HashMap<String, JsonSchemaEntry>>,
 
+    // enum's name
+    // DynamicMessage will transform name to value
     #[serde(rename = "enum", skip_serializing_if = "Option::is_none")]
     enum_: Option<Vec<String>>,
 
-    /// Don't allow any other properties in the object
+    // Don't allow any other properties in the object
+    #[serde(skip_serializing_if = "skip_serial_if_false")]
     additional_properties: bool,
 
-    /// Set all properties to required
+    // Set all properties to required
     #[serde(skip_serializing_if = "Option::is_none")]
     required: Option<Vec<String>>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     items: Option<Box<JsonSchemaEntry>>,
+
+    #[serde(skip_serializing_if = "Option::is_none", rename = "$defs")]
+    defs: Option<HashMap<String, JsonSchemaEntry>>,
+
+    #[serde(skip_serializing_if = "Option::is_none", rename = "$ref")]
+    ref_: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    one_of: Option<Vec<JsonSchemaEntry>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pattern_properties: Option<HashMap<String, JsonSchemaEntry>>,
+}
+
+impl JsonSchemaEntry {
+    pub fn object() -> Self {
+        JsonSchemaEntry {
+            type_: Some(JsonType::Object),
+            properties: Some(HashMap::new()),
+            ..Default::default()
+        }
+    }
+    pub fn boolean() -> Self {
+        JsonSchemaEntry {
+            type_: Some(JsonType::Boolean),
+            ..Default::default()
+        }
+    }
+    pub fn number() -> Self {
+        JsonSchemaEntry {
+            type_: Some(JsonType::Number),
+            ..Default::default()
+        }
+    }
+
+    pub fn string() -> Self {
+        JsonSchemaEntry {
+            type_: Some(JsonType::String),
+            ..Default::default()
+        }
+    }
+
+    pub fn bytes_string() -> Self {
+        JsonSchemaEntry {
+            type_: Some(JsonType::String),
+            ..Default::default()
+        }
+    }
+    pub fn reference(ref_: String) -> Self {
+        JsonSchemaEntry {
+            ref_: Some(ref_),
+            ..Default::default()
+        }
+    }
+    pub fn array(item: JsonSchemaEntry) -> Self {
+        JsonSchemaEntry {
+            type_: Some(JsonType::Array),
+            items: Some(Box::new(item)),
+            ..Default::default()
+        }
+    }
+    pub fn enums(enums: Vec<String>) -> Self {
+        JsonSchemaEntry {
+            enum_: Some(enums),
+            ..Default::default()
+        }
+    }
+    pub fn one_of(one_of: Vec<JsonSchemaEntry>) -> Self {
+        JsonSchemaEntry {
+            one_of: Some(one_of),
+            ..Default::default()
+        }
+    }
+    pub fn pattern_properties(pattern: String, property: JsonSchemaEntry) -> Self {
+        let mut map = HashMap::new();
+        map.insert(pattern, property);
+        JsonSchemaEntry {
+            pattern_properties: Some(map),
+            ..Default::default()
+        }
+    }
+}
+
+fn skip_serial_if_false(b: &bool) -> bool {
+    !*b
+}
+
+fn field_to_type_or_ref(root_name: &str, field: FieldDescriptor) -> JsonSchemaEntry {
+    match field.kind() {
+        prost_reflect::Kind::Bool => JsonSchemaEntry::boolean(),
+        prost_reflect::Kind::Double
+        | prost_reflect::Kind::Float
+        | prost_reflect::Kind::Int32
+        | prost_reflect::Kind::Int64
+        | prost_reflect::Kind::Uint32
+        | prost_reflect::Kind::Uint64
+        | prost_reflect::Kind::Sint32
+        | prost_reflect::Kind::Sint64
+        | prost_reflect::Kind::Fixed32
+        | prost_reflect::Kind::Fixed64
+        | prost_reflect::Kind::Sfixed32
+        | prost_reflect::Kind::Sfixed64 => JsonSchemaEntry::number(),
+        prost_reflect::Kind::String => JsonSchemaEntry::string(),
+        prost_reflect::Kind::Bytes => JsonSchemaEntry::bytes_string(),
+        prost_reflect::Kind::Enum(enums) => {
+            let values = enums.values().map(|v| v.name().to_string()).collect::<Vec<_>>();
+            JsonSchemaEntry::enums(values)
+        }
+        prost_reflect::Kind::Message(fm) => {
+            if root_name == fm.full_name() {
+                JsonSchemaEntry::reference("#".to_string())
+            } else {
+                JsonSchemaEntry::reference(format!("#/definitions/{}", fm.full_name()))
+            }
+        }
+    }
+}
+
+fn is_message_field(field: &FieldDescriptor) -> Option<MessageDescriptor> {
+    match field.kind() {
+        prost_reflect::Kind::Message(m) => Some(m),
+        _ => None,
+    }
+}
+
+fn map_key_pattern(field: &FieldDescriptor) -> &'static str {
+    match field.kind() {
+        prost_reflect::Kind::Bool => "^(true|false)$",
+        prost_reflect::Kind::Int32
+        | prost_reflect::Kind::Int64
+        | prost_reflect::Kind::Uint32
+        | prost_reflect::Kind::Uint64
+        | prost_reflect::Kind::Sint32
+        | prost_reflect::Kind::Sint64
+        | prost_reflect::Kind::Fixed32
+        | prost_reflect::Kind::Fixed64
+        | prost_reflect::Kind::Sfixed32
+        | prost_reflect::Kind::Sfixed64 => "^[0-9]+$",
+        prost_reflect::Kind::String => ".*",
+        _ => {
+            unreachable!("map key pattern for {:?}", field.kind());
+        }
+    }
 }
 
 enum JsonType {
@@ -38,7 +184,6 @@ enum JsonType {
     Object,
     Array,
     Boolean,
-    Null,
     _UNKNOWN,
 }
 
@@ -59,121 +204,151 @@ impl serde::Serialize for JsonType {
             JsonType::Object => serializer.serialize_str("object"),
             JsonType::Array => serializer.serialize_str("array"),
             JsonType::Boolean => serializer.serialize_str("boolean"),
-            JsonType::Null => serializer.serialize_str("null"),
             JsonType::_UNKNOWN => serializer.serialize_str("unknown"),
         }
     }
 }
 
-impl<'de> serde::Deserialize<'de> for JsonType {
-    fn deserialize<D>(deserializer: D) -> Result<JsonType, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        match s.as_str() {
-            "string" => Ok(JsonType::String),
-            "number" => Ok(JsonType::Number),
-            "object" => Ok(JsonType::Object),
-            "array" => Ok(JsonType::Array),
-            "boolean" => Ok(JsonType::Boolean),
-            "null" => Ok(JsonType::Null),
-            _ => Ok(JsonType::_UNKNOWN),
+pub fn message_to_json_schema(_: &DescriptorPool, root_msg: MessageDescriptor) -> JsonSchemaEntry {
+    let root_name = root_msg.full_name().to_string();
+
+    let mut msg_queue = std::collections::VecDeque::new();
+    msg_queue.push_back(root_msg.clone());
+    let mut msg_mapping = HashMap::<String, JsonSchemaEntry>::new();
+
+    // level traversal, to make sure all message type is defined before used
+    while msg_queue.len() > 0 {
+        let msg = msg_queue.pop_front().unwrap();
+        let name = msg.full_name().to_string();
+        if msg_mapping.contains_key(&name) {
+            continue;
+        }
+
+        msg_mapping.insert(name.clone(), JsonSchemaEntry::object());
+        for child in msg.child_messages() {
+            let name = child.full_name().to_string();
+            if msg_mapping.contains_key(&name) {
+                continue;
+            }
+            msg_mapping.insert(name, JsonSchemaEntry::object());
+            msg_queue.push_back(child);
+        }
+
+        for field in msg.fields() {
+            if let Some(mut fm) = is_message_field(&field) {
+                let mut msg_full_name = fm.full_name().to_string();
+                if field.is_map() {
+                    // for field with map<key, value> type, there will be a *Entry type generated
+                    // key is always scalar type, so no need to process
+                    // value can be any type, so need to unpack value type
+                    let value_field = fm.get_field_by_name("value").unwrap();
+                    if let Some(value_fm) = is_message_field(&value_field) {
+                        msg_full_name = value_fm.full_name().to_string();
+                        fm = value_fm;
+                    } else {
+                        // map, but value is not message
+                        // no need to process
+                        msg_full_name = String::new();
+                    }
+                }
+                // skip if map value is not message
+                if msg_full_name.is_empty() {
+                    continue;
+                }
+                if msg_mapping.contains_key(&msg_full_name) {
+                    continue;
+                }
+                msg_mapping.insert(msg_full_name, JsonSchemaEntry::object());
+                msg_queue.push_back(fm);
+            }
         }
     }
-}
 
-pub fn message_to_json_schema(
-    pool: &DescriptorPool,
-    message: MessageDescriptor,
-) -> JsonSchemaEntry {
-    let mut schema = JsonSchemaEntry {
-        title: Some(message.name().to_string()),
-        type_: JsonType::Object, // Messages are objects
-        ..Default::default()
-    };
+    // now to fill in the properties
+    let mut visited = std::collections::HashSet::new();
+    msg_queue.push_back(root_msg);
+    while msg_queue.len() > 0 {
+        let msg = msg_queue.pop_front().unwrap();
+        if visited.contains(msg.full_name()) {
+            continue;
+        }
+        visited.insert(msg.full_name().to_string());
 
-    let mut properties = HashMap::new();
-    message.fields().for_each(|f| match f.kind() {
-        prost_reflect::Kind::Message(m) => {
-            properties.insert(f.name().to_string(), message_to_json_schema(pool, m));
-        }
-        prost_reflect::Kind::Enum(e) => {
-            properties.insert(
-                f.name().to_string(),
-                JsonSchemaEntry {
-                    type_: map_proto_type_to_json_type(f.field_descriptor_proto().r#type()),
-                    enum_: Some(e.values().map(|v| v.name().to_string()).collect::<Vec<_>>()),
-                    ..Default::default()
-                },
-            );
-        }
-        _ => {
-            // TODO: Handle repeated label
-            match f.field_descriptor_proto().label() {
-                field_descriptor_proto::Label::Repeated => {
-                    // TODO: Handle more complex repeated types. This just handles primitives for now
-                    properties.insert(
-                        f.name().to_string(),
-                        JsonSchemaEntry {
-                            type_: JsonType::Array,
-                            items: Some(Box::new(JsonSchemaEntry {
-                                type_: map_proto_type_to_json_type(
-                                    f.field_descriptor_proto().r#type(),
+        let entry = msg_mapping.get_mut(msg.full_name()).unwrap();
+
+        for field in msg.fields() {
+            let field_name = field.name().to_string();
+
+            if let Some(oneof) = field.containing_oneof(){
+                entry.properties.as_mut().unwrap().insert(
+                    field_name,
+                    JsonSchemaEntry::one_of(
+                        oneof.fields().map(|f| {
+                            // hack
+                            if let Some(fm) = is_message_field(&f){
+                                msg_queue.push_back(fm);
+                            }
+                            // fields of any type, except map fields and repeated fields
+                            // so convert directly is ok
+                            field_to_type_or_ref(&root_name, f)
+                        }).collect::<Vec<_>>()
+                    )
+                );
+                continue;
+            }
+
+            let (field_type, nest_msg) = {
+                if let Some(fm) = is_message_field(&field) {
+                    if field.is_list() {
+                        // repeated message type
+                        (JsonSchemaEntry::array(field_to_type_or_ref(&root_name, field)), Some(fm))
+                    } else if field.is_map() {
+                        let key_field = fm.get_field_by_name("key").unwrap();
+                        let value_field = fm.get_field_by_name("value").unwrap();
+
+                        let key_pattern = map_key_pattern(&key_field);
+                        if let Some(fm) = is_message_field(&value_field) {
+                            (
+                                JsonSchemaEntry::pattern_properties(
+                                    key_pattern.to_string(),
+                                    field_to_type_or_ref(&root_name, value_field),
                                 ),
-                                ..Default::default()
-                            })),
-                            ..Default::default()
-                        },
-                    );
-                }
-                _ => {
-                    // Regular JSON field
-                    properties.insert(
-                        f.name().to_string(),
-                        JsonSchemaEntry {
-                            type_: map_proto_type_to_json_type(f.field_descriptor_proto().r#type()),
-                            ..Default::default()
-                        },
-                    );
+                                Some(fm),
+                            )
+                        } else {
+                            (
+                                JsonSchemaEntry::pattern_properties(
+                                    key_pattern.to_string(),
+                                    field_to_type_or_ref(&root_name, value_field),
+                                ),
+                                None,
+                            )
+                        }
+                    } else {
+                        (field_to_type_or_ref(&root_name, field), Some(fm))
+                    }
+                } else {
+                    if field.is_list() {
+                        // repeated scalar type
+                        (JsonSchemaEntry::array(field_to_type_or_ref(&root_name, field)), None)
+                    } else {
+                        (field_to_type_or_ref(&root_name, field), None)
+                    }
                 }
             };
+
+            if let Some(fm) = nest_msg {
+                msg_queue.push_back(fm);
+            }
+
+            entry.properties.as_mut().unwrap().insert(field_name, field_type);
         }
-    });
-
-    schema.properties = Some(properties);
-
-    // All proto 3 fields are optional, so maybe we could
-    // make this a setting?
-    // schema.required = Some(
-    //     message
-    //         .fields()
-    //         .map(|f| f.name().to_string())
-    //         .collect::<Vec<_>>(),
-    // );
-
-    schema
-}
-
-fn map_proto_type_to_json_type(proto_type: field_descriptor_proto::Type) -> JsonType {
-    match proto_type {
-        field_descriptor_proto::Type::Double => JsonType::Number,
-        field_descriptor_proto::Type::Float => JsonType::Number,
-        field_descriptor_proto::Type::Int64 => JsonType::Number,
-        field_descriptor_proto::Type::Uint64 => JsonType::Number,
-        field_descriptor_proto::Type::Int32 => JsonType::Number,
-        field_descriptor_proto::Type::Fixed64 => JsonType::Number,
-        field_descriptor_proto::Type::Fixed32 => JsonType::Number,
-        field_descriptor_proto::Type::Bool => JsonType::Boolean,
-        field_descriptor_proto::Type::String => JsonType::String,
-        field_descriptor_proto::Type::Group => JsonType::_UNKNOWN,
-        field_descriptor_proto::Type::Message => JsonType::Object,
-        field_descriptor_proto::Type::Bytes => JsonType::String,
-        field_descriptor_proto::Type::Uint32 => JsonType::Number,
-        field_descriptor_proto::Type::Enum => JsonType::String,
-        field_descriptor_proto::Type::Sfixed32 => JsonType::Number,
-        field_descriptor_proto::Type::Sfixed64 => JsonType::Number,
-        field_descriptor_proto::Type::Sint32 => JsonType::Number,
-        field_descriptor_proto::Type::Sint64 => JsonType::Number,
     }
+
+    let mut root = msg_mapping.remove(&root_name).unwrap();
+    if msg_mapping.len() > 0 {
+        root.defs = Some(msg_mapping);
+    }
+
+    root
 }
