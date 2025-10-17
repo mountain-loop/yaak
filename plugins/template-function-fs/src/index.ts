f5f635850c525a4b7bdd52b03fdad5ec5e547867
Maksim Karelov <max.karelov@gmail.com>
2025-10-17 20:53:11 +0700

    Add ability to select `fs.readFile` encoding

--- plugins/template-function-fs/src/index.ts
+++ plugins/template-function-fs/src/index.ts
@@ -1,17 +1,47 @@
 import type { CallTemplateFunctionArgs, Context, PluginDefinition } from '@yaakapp/api';
 import fs from 'node:fs';
 
+const options = [
+  'ascii',
+  'utf8',
+  'utf-8',
+  'utf16le',
+  'utf-16le',
+  'ucs2',
+  'ucs-2',
+  'base64',
+  'base64url',
+  'latin1',
+  'binary',
+  'hex',
+].map((it) => ({
+  label: it,
+  value: it,
+}));
+
 export const plugin: PluginDefinition = {
   templateFunctions: [
     {
       name: 'fs.readFile',
       description: 'Read the contents of a file as utf-8',
-      args: [{ title: 'Select File', type: 'file', name: 'path', label: 'File' }],
+      args: [
+        { title: 'Select File', type: 'file', name: 'path', label: 'File' },
+        {
+          title: 'Select encoding',
+          type: 'select',
+          name: 'encoding',
+          label: 'Encoding',
+          options,
+          defaultValue: 'utf-8',
+        },
+      ],
       async onRender(_ctx: Context, args: CallTemplateFunctionArgs): Promise<string | null> {
-        if (!args.values.path) return null;
+        if (!args.values.path || !args.values.encoding) return null;
 
         try {
-          return fs.promises.readFile(String(args.values.path ?? ''), 'utf-8');
+          return fs.promises.readFile(String(args.values.path ?? ''), {
+            encoding: String(args.values.encoding ?? 'utf-8') as BufferEncoding,
+          });
         } catch {
           return null;
         }
