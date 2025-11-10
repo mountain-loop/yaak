458df4854b77601897afeceee6cdf84627b70980
Gregory Schier <gschier1990@gmail.com>
2025-11-10 14:34:04 -0800

    Clean up old HTTP connections

--- src-tauri/yaak-http/src/manager.rs
+++ src-tauri/yaak-http/src/manager.rs
@@ -4,39 +4,37 @@ use log::info;
 use reqwest::Client;
 use std::collections::BTreeMap;
 use std::sync::Arc;
+use std::time::{Duration, Instant};
 use tokio::sync::RwLock;
 
 pub struct HttpConnectionManager {
-    connections: Arc<RwLock<BTreeMap<String, Client>>>,
+    connections: Arc<RwLock<BTreeMap<String, (Client, Instant)>>>,
+    ttl: Duration,
 }
 
 impl HttpConnectionManager {
     pub fn new() -> Self {
         Self {
             connections: Arc::new(RwLock::new(BTreeMap::new())),
+            ttl: Duration::from_mins(10),
         }
     }
 
     pub async fn get_client(&self, id: &str, opt: &HttpConnectionOptions) -> Result<Client> {
         let mut connections = self.connections.write().await;
-        if let Some(c) = connections.get(id).cloned() {
+
+        // Clean old connections
+        connections.retain(|_, (_, last_used)| last_used.elapsed() <= self.ttl);
+
+        if let Some((c, last_used)) = connections.get(id).cloned() {
             info!("Re-using HTTP client {id}");
+            *last_used = Instant::now();
             return Ok(c);
         }
 
         info!("Building new HTTP client {id}");
         let c = opt.build_client()?;
-        connections.insert(id.into(), c.clone());
+        connections.insert(id.into(), (c.clone(), Instant::now()));
         Ok(c)
-    }
-
-    pub async fn add_client(&self, id: impl Into<String>, client: Client) {
-        let mut connections = self.connections.write().await;
-        connections.insert(id.into(), client);
-    }
-
-    pub async fn remove_client(&self, id: &str) {
-        let mut connections = self.connections.write().await;
-        connections.remove(id);
     }
 }
