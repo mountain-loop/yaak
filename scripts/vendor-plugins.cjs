5465efea84d165c530a41b7cdc725aa0812f9954
Gregory Schier <gschier1990@gmail.com>
2025-11-18 08:53:55 -0800

    Don't build in vendor-plugins

--- scripts/vendor-plugins.cjs
+++ scripts/vendor-plugins.cjs
@@ -1,6 +1,5 @@
 const { readdirSync, cpSync } = require('node:fs');
 const path = require('node:path');
-const { execSync } = require('node:child_process');
 
 const pluginsDir = path.join(__dirname, '..', 'plugins');
 
@@ -9,7 +8,6 @@ console.log('Copying Yaak plugins to', pluginsDir);
 for (const name of readdirSync(pluginsDir)) {
   const dir = path.join(pluginsDir, name);
   if (name.startsWith('.')) continue;
-  execSync('npm run build', { cwd: dir });
   const destDir = path.join(__dirname, '../src-tauri/vendored/plugins/', name);
   console.log(`Copying ${name} to ${destDir}`);
   cpSync(path.join(dir, 'package.json'), path.join(destDir, 'package.json'));
