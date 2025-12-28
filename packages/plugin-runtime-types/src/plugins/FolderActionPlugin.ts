8e1959b7c390e7a687003d1ed4f328c7732696d7
Gregory Schier <gschier1990@gmail.com>
2025-12-28 15:06:18 -0800

    Use generated types for FolderActionPlugin and WorkspaceActionPlugin

--- packages/plugin-runtime-types/src/plugins/FolderActionPlugin.ts
+++ packages/plugin-runtime-types/src/plugins/FolderActionPlugin.ts
@@ -1,10 +1,5 @@
+import type { CallFolderActionArgs, FolderAction } from '../bindings/gen_events';
 import type { Context } from './Context';
-import type { Folder } from '../bindings/gen_models';
-import type { Icon } from '../bindings/gen_events';
-
-export type FolderAction = { label: string; icon?: Icon };
-
-export type CallFolderActionArgs = { folder: Folder };
 
 export type FolderActionPlugin = FolderAction & {
   onSelect(ctx: Context, args: CallFolderActionArgs): Promise<void> | void;
--- packages/plugin-runtime-types/src/plugins/WorkspaceActionPlugin.ts
+++ packages/plugin-runtime-types/src/plugins/WorkspaceActionPlugin.ts
@@ -1,10 +1,5 @@
+import type { CallWorkspaceActionArgs, WorkspaceAction } from '../bindings/gen_events';
 import type { Context } from './Context';
-import type { Workspace } from '../bindings/gen_models';
-import type { Icon } from '../bindings/gen_events';
-
-export type WorkspaceAction = { label: string; icon?: Icon };
-
-export type CallWorkspaceActionArgs = { workspace: Workspace };
 
 export type WorkspaceActionPlugin = WorkspaceAction & {
   onSelect(ctx: Context, args: CallWorkspaceActionArgs): Promise<void> | void;
